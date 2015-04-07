e4306d6def697c114a65eaeefef9637da33cf76c
Ian Neubert <ian@ianneubert.com>
2015-04-07 11:59:43 -0700

    Test circle Ci and add first api test call.

--- server/halfpipe/api.rb
+++ server/halfpipe/api.rb
@@ -1,4 +1,5 @@
 require 'grape'
+require 'json'
 
 module Halfpipe
   class APIv1 < Grape::API
@@ -7,6 +8,16 @@ module Halfpipe
 
     get :ping do
       "ok"
+    end
+
+    post :recieve_hook do
+      payload = JSON.parse params["payload"]
+
+      sha1 = payload['commits'].last['raw_node']
+      url = "#{payload['canon_url']}#{payload['repository']['absolute_url'][0..-2]}.git"
+      branch = payload['commits'].last['branch']
+
+      {sha1: sha1, url: url, branch: branch}
     end
   end
 end
